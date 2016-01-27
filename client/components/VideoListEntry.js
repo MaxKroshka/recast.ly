7c02d92f6348d90fdadd20361c30ef85858da4dc
MaxKroshka <max.kroshka@gmail.com>
2016-01-27 15:29:23 -0800

    implement VideoListEntry as a component that dynamically renders example video data

--- client/components/VideoListEntry.js
+++ client/components/VideoListEntry.js
@@ -1,13 +1,21 @@
-var VideoListEntry = () => (
-  <div className="video-list-entry">
-    <div className="media-left media-middle">
-      <img className="media-object" src="//i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" />
+class VideoListEntry extends React.Component {
+ constructor (props) {
+  super(props)
+ }
+
+ render () { 
+  return (
+    <div className="video-list-entry">
+      <div className="media-left media-middle">
+        <img className="media-object" src={this.props.video.snippet.thumbnails.default.url} alt="" />
+      </div>
+      <div className="media-body">
+        <div className="video-list-entry-title">Video Title</div>
+        <div className="video-list-entry-detail">Video Description</div>
+      </div>
     </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
-    </div>
-  </div>
-);
+    )
+  }
+};
 
 window.VideoListEntry = VideoListEntry;
