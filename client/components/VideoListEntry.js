ac15088e755d8fd14e832359566179976dad5913
MaxKroshka <max.kroshka@gmail.com>
2016-01-27 16:00:33 -0800

    Refactor VideoPlayer component to ES6 standard

--- client/components/VideoListEntry.js
+++ client/components/VideoListEntry.js
@@ -10,8 +10,8 @@ class VideoListEntry extends React.Component {
         <img className="media-object" src={this.props.video.snippet.thumbnails.default.url} alt="" />
       </div>
       <div className="media-body">
-        <div className="video-list-entry-title">Video Title</div>
-        <div className="video-list-entry-detail">Video Description</div>
+        <div className="video-list-entry-title">{this.props.video.snippet.title}</div>
+        <div className="video-list-entry-detail">{this.props.video.snippet.description}</div>
       </div>
     </div>
     )
